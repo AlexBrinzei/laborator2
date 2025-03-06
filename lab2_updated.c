6bc74b5588e3197deff46e44a9155db42a0805e9
AlexBrinzei <mihai.brinzei@stud.acs.upb.ro>
2025-03-06 12:53:18 +0200

    updated code

---
+++ lab2_updated.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int function(int x){
+	if(x <= 0)
+		return x;
+	x = x-2;
+	return function(x);
+	
+}
+
+int main(){
+	int a=30, i=0;
+	
+	while (a>0){
+		a=function(a);
+		i++;
+	}
+	printf("%d", i);
+	return 0;	
+}
+
